563ced9bf46c7abbc7af8dd64380596c644fa20c
Luca Park <qlife1146@gmail.com>
2025-05-13 17:32:53 +0900

    Complete

--- Counter_SU/ContentView.swift
+++ Counter_SU/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    let spacing: CGFloat = 60.0
+    
+    
+    @State private var counterValue: Int = 0 {
+        didSet {}
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        GeometryReader { geo in ZStack {
+            Text("\(counterValue)")
+                .font(.system(size: 33))
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .multilineTextAlignment(.center)
+            
+            HStack(spacing: spacing) {
+                Button(action: {
+                    if counterValue > -10 {
+                        counterValue -= 1
+                    }                }) {
+                    Text("-")
+                        .font(.system(size: 25))
+                }
+                .frame(width: 50, height: 50)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+
+
+                Button(action: {
+                    counterValue = 0
+                }) {
+                    Text("0")
+                        .font(.system(size: 25))
+                }
+                .frame(width: 50, height: 50)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+
+
+                Button(action: {
+                    if counterValue < 10 {
+                        counterValue += 1
+                    }
+                }) {
+                    Text("+")
+                        .font(.system(size: 25))
+                }
+                .frame(width: 50, height: 50)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+            }
+            .position(x: geo.size.width / 2,
+                      y: geo.size.height - geo.safeAreaInsets.bottom - 50)
+
+        }}
     }
 }
 
