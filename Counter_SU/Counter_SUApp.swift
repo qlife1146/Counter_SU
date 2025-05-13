e8eb2e990ed5126b16de0f43f3455a84ee1f5710
Luca Park <qlife1146@gmail.com>
2025-05-13 15:37:11 +0900

    Initial Commit

---
+++ Counter_SU/Counter_SUApp.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_SUApp.swift
+//  Counter_SU
+//
+//  Created by Luca Park on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Counter_SUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
